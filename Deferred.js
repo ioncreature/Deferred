5cfa3e01d37e425c2e021ff86da1321fba12a85c
Marenin Alex <great_muchacho@mail.ru>
2012-07-05 14:43:47 +0400

    added tests

--- Deferred.js
+++ Deferred.js
@@ -251,29 +251,7 @@
 	// module:
 	//		dojo/when
 	var when = (function(Deferred, Promise){
-		"use strict";
 		return function when(valueOrPromise, /*Function?*/ callback, /*Function?*/ errback, /*Function?*/ progback){
-			// summary:
-			//		Transparently applies callbacks to values and/or promises.
-			// description:
-			//		Accepts promises but also transparently handles non-promises. If no
-			//		callbacks are provided returns a promise, regardless of the initial
-			//		value. Foreign promises are converted.
-			//		If callbacks are provided and the initial value is not a promise,
-			//		the callback is executed immediately with no error handling. Returns
-			//		a promise if the initial value is a promise, or the result of the
-			//		callback otherwise.
-			// returns: dojo/promise/Promise
-			//
-			// valueOrPromise:
-			//		Either a regular value or a promise.
-			// callback:
-			//		Callback to be invoked when the promise is resolved, or a non-promise
-			//		is received.
-			// errback:
-			//		Callback to be invoked when the promise is rejected.
-			// progback:
-			//		Callback to be invoked when the promise emits a progress update.
 			var receivedPromise = valueOrPromise && typeof valueOrPromise.then === "function";
 			var nativePromise = receivedPromise && valueOrPromise instanceof Promise;
 
@@ -295,7 +273,7 @@
 			return valueOrPromise;
 		};
 	})();
-
+	Deferred.when = when;
 
 
 	if ( typeof define == 'function' && define.amd )
--- test/Deferred-test.js
+++ test/Deferred-test.js
@@ -3,19 +3,74 @@
  * June 2012
  */
 
+function isFn( a ){
+	return typeof a === 'function';
+}
+
 module( 'Deferred creation' );
 
 test( 'Simple class creation', function(){
 	ok( typeof Deferred == 'function', 'Deferred constrictor exists' );
 
-	ok( def, 'Deferred object is created' );
-	ok( def.then, 'method .then exists' );
-	ok( def.reject, 'method .reject exists' );
-	ok( def.resolve, 'method .resolve exists' );
-	ok( def.progress, 'method .progress exists' );
-	ok( def.cancel, 'method .cancel exists' );
-	ok( def.isRejected, 'method .isRejected exists' );
-	ok( def.isResolved, 'method .isResolved exists' );
-	ok( def.isFulfilled, 'method .isFulfilled exists' );
-	ok( def.isCanceled, 'method .isCanceled exists' );
+	var d = new Deferred();
+
+	ok( d, 'Deferred object is created' );
+	ok( isFn(d.then), 'method .then exists' );
+	ok( isFn(d.reject), 'method .reject exists' );
+	ok( isFn(d.resolve), 'method .resolve exists' );
+	ok( isFn(d.progress), 'method .progress exists' );
+	ok( isFn(d.cancel), 'method .cancel exists' );
+	ok( isFn(d.isRejected), 'method .isRejected exists' );
+	ok( isFn(d.isResolved), 'method .isResolved exists' );
+	ok( isFn(d.isFulfilled), 'method .isFulfilled exists' );
+	ok( isFn(d.isCanceled), 'method .isCanceled exists' );
+});
+
+
+test( 'Resolve test', function(){
+	var d = new Deferred();
+
+	stop();
+
+	d.then( function( arg1, arg2 ){
+		equal( arg1, 10, 'Resolve passes arguments' );
+		start();
+	});
+
+	setTimeout( function(){
+		d.resolve( 10 );
+		ok( d.isResolved(), 'isResolved' );
+		ok( d.isFulfilled(), 'isFulfilled' );
+	}, 1 );
+});
+
+
+test( 'Reject test', function(){
+	var d = new Deferred(),
+		d2 = new Deferred();
+
+	stop( 2 );
+
+	d.then( null, function( error ){
+		ok( error instanceof Error, 'Reject passes error' );
+		start();
+	});
+
+	d2.then( null, function(){
+		start();
+	});
+
+	setTimeout( function(){
+		d.reject( new Error() );
+		d2.reject();
+		ok( d.isRejected(),  'd isResolved' );
+		ok( d.isFulfilled(), 'd isFulfilled' );
+		ok( d2.isRejected(),  'd2 isResolved' );
+		ok( d2.isFulfilled(), 'd2 isFulfilled' );
+	}, 1 );
+});
+
+
+test( 'Deferred.when tests', function(){
+	ok( true, 'skip' );
 });
